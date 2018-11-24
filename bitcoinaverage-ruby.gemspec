8fbb17d851c3b06767cca7fe9c7a291fc8a3f4a4
Nassima Kara <nassimakara@icloud.com>
2018-11-24 19:44:02 +0000

    First Commit

---
+++ bitcoinaverage-ruby.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'bitcoinaverage-ruby'
+  s.version = '0.0.1-alpha'
+  s.date = '2018-11-24'
+  s.summary = 'A simple Ruby wrapper for BitCoin Average API'
+  s.authors = ['Nassima Kara']
+  s.email = ['nassima.kara@outlook.com']
+  s.files = ['lib/bitcoinaverage-ruby.rb']
+  s.homepage = 'https://github.com/nassimakaracloud/bitcoinaverage-ruby'
+  s.license = 'MIT'
+  
+  s.add_dependency 'httparty', '0.16.3'
+end
---
+++ lib/bitcoinaverage-ruby.rb
@@ -0,0 +1,24 @@
+require 'httparty'
+
+module BitcoinAverage
+  class HTTP
+    include HTTParty
+    
+    base_uri "https://apiv2.bitcoinaverage.com"
+    
+    def initialize
+    end
+    
+    def make_call(path, options = {})
+      method = options[:method] || :get
+      self.class.send(method, path)
+    end
+    
+    def ticker_data(symbol_set, symbol)
+        path = "/indices/#{symbol_set}/ticker/#{symbol}"
+        make_call path
+    end
+    
+   
+  end
+end
